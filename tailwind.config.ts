d9139ced0f5f723e2eed77f49dd46fb02f1ac0e6
Bruno Gallotte <brunogallotte1@gmail.com>
2024-01-11 09:02:07 -0300

    chore: initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,7 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.tsx',
   ],
   theme: {
     extend: {
@@ -13,6 +11,15 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        varosPrincipalDark: '#131313',
+        varosPrincipalGreen: '#19C819',
+        greyPaletteC1: '#F2F4F8',
+        greyPaletteC2: '#E2E5EB',
+        greyPaletteC5: '#B0B7BE',
+        greyPaletteC8: '#222729',
+        greyPaletteC9: '#131516'
+      }
     },
   },
   plugins: [],
